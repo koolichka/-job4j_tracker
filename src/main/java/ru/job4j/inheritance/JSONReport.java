37425caa67ed46aab9723b1c10951976ed07b583
koolichka@mail.ru <QFR-svw-kTD-scP>
2022-03-04 18:50:08 +0300

    5. Аннотация @Override [#174891]

--- src/main/java/ru/job4j/inheritance/JSONReport.java
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -1,6 +1,8 @@
 package ru.job4j.inheritance;
 
 public class JSONReport extends TextReport {
+
+    @Override
     public String generate(String name, String body) {
         String sls = System.lineSeparator();
         return "{" + sls + "\t\"name\" : \"" + name + "\","
