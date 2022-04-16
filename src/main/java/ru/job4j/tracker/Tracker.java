e850227ad37743a77f2e102cdaa349702d9ca288
koolichka@mail.ru <QFR-svw-kTD-scP>
2022-04-16 13:53:52 +0300

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -9,6 +11,32 @@ public class Tracker {
         item.setId(ids++);
         items[size++] = item;
         return item;
+    }
+
+    public Item[] findAll() {
+        Item[] rsl = new Item[size];
+        int resize = 0;
+        for (int index = 0; index < size; index++) {
+            Item name = items[index];
+            if (name != null) {
+                rsl[resize] = name;
+                resize++;
+            }
+        }
+        return Arrays.copyOf(rsl, size);
+    }
+
+    public Item[] findByName(String key) {
+        Item[] rsl = new Item[size];
+        int resize = 0;
+        for (int index = 0; index < size; index++) {
+            Item name = items[index];
+            if (name.getName().equals(key)) {
+                rsl[index] = name;
+                resize++;
+            }
+        }
+       return Arrays.copyOf(rsl, resize);
     }
 
     public Item findById(int id) {
