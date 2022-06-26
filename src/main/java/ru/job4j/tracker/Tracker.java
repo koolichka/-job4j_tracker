24c53a86b6bab1acc534d44a2809895817dc7acc
koolichka@mail.ru <QFR-svw-kTD-scP>
2022-06-26 11:42:17 +0300

    7. Метод удаления заявки Tracker.delete [#211749 #310732]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -56,14 +56,13 @@ public class Tracker {
     }
 
     public boolean delete(int id) {
+        int index = indexOf(id);
         if (indexOf(id) != -1) {
-            int index = indexOf(id);
             System.arraycopy(items, index + 1, items, index, size - index);
             items[size - 1] = null;
             size--;
             return true;
-        } else {
-            return false;
         }
+            return false;
     }
 }
