631d44d41aac174ba727315dde5d3abcd5c7154b
koolichka@mail.ru <QFR-svw-kTD-scP>
2022-08-20 17:15:45 +0300

    1.1. Тесты на StartUI. [#223313]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -152,4 +152,24 @@ public class StartUITest {
                         + "Exit program" + ln
         );
     }
+
+    @Test
+    public void whenInvalidExit() {
+        Output out = new StubOutput();
+        Input in = new StubInput(new String[] {"11", "0"});
+        Tracker tracker = new Tracker();
+        UserAction[] actions = new UserAction[]{
+                new ExitAction(out)
+        };
+        new StartUI(out).init(in, tracker, actions);
+        String ln = System.lineSeparator();
+        assertThat(out.toString()).isEqualTo(
+                "Menu." + ln
+                        + "0. Exit program" + ln
+                        + "Wrong input, you can select: 0 .." + (actions.length - 1) + ln
+                        + "Menu." + ln
+                        + "0. Exit program" + ln
+                        + "Exit program" + ln
+        );
+    }
 }
