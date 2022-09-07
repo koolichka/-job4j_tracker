23617ffa408d76cdfcdb7e86cd6961a1f9704c27
koolichka@mail.ru <QFR-svw-kTD-scP>
2022-09-07 11:39:03 +0300

    4. Принципы раннего возврата и охранных выражений [#504875 #338992]

--- src/main/java/ru/job4j/early/PasswordValidator.java
+++ src/main/java/ru/job4j/early/PasswordValidator.java
@@ -3,59 +3,57 @@ package ru.job4j.early;
 public class PasswordValidator {
     public static String validate(String password) {
 
-        boolean hasDigit = true;
+        boolean hasDigit = false;
         boolean hasLetterOrDigit = false;
         boolean forbiddenValues = true;
         String[] invalidPassword = {"qwerty", "12345", "password", "admin", "user"};
 
-        if (password != null) {
+        if (password == null) {
+            throw new IllegalArgumentException("Password is null.");
+        }
 
-            if (password.length() < 8 || password.length() > 32) {
-                throw new IllegalArgumentException(
-                        "The password length cannot be less than 8 or more than 32 characters.");
-            }
-            if (password.equals(password.toLowerCase())) {
-                throw new IllegalArgumentException(
-                        "The password must contain uppercase character.");
-            }
-            if (password.equals(password.toUpperCase())) {
-                throw new IllegalArgumentException(
-                        "The password must contain lowercase character.");
-            }
+        if (password.length() < 8 || password.length() > 32) {
+            throw new IllegalArgumentException(
+                    "The password length cannot be less than 8 or more than 32 characters.");
+        }
+        if (password.equals(password.toLowerCase())) {
+            throw new IllegalArgumentException(
+                    "The password must contain uppercase character.");
+        }
+        if (password.equals(password.toUpperCase())) {
+            throw new IllegalArgumentException(
+                    "The password must contain lowercase character.");
+        }
+        for (int index = 0; index < password.length(); index++) {
+            char ch = password.charAt(index);
             for (String pass : invalidPassword) {
                 if (password.toLowerCase().contains(pass.toLowerCase())) {
                     forbiddenValues = false;
                     break;
                 }
             }
-            for (int index = 0; index < password.length(); index++) {
-                char ch = password.charAt(index);
-                if (Character.isDigit(ch)) {
-                    hasDigit = false;
-                    break;
-                }
+            if (Character.isDigit(ch)) {
+                hasDigit = true;
             }
-            for (int index = 0; index < password.length(); index++) {
-                char ch = password.charAt(index);
-                if (!Character.isLetterOrDigit(ch)) {
-                    hasLetterOrDigit = true;
-                    break;
-                }
+            if (!Character.isLetterOrDigit(ch)) {
+                hasLetterOrDigit = true;
             }
-            if (hasDigit) {
-                throw new IllegalArgumentException(
-                        "The password must contain a digit.");
+            if (hasDigit && hasLetterOrDigit && forbiddenValues) {
+                break;
             }
-            if (!hasLetterOrDigit) {
-                throw new IllegalArgumentException(
-                        "The password must contain special character.");
-            }
-            if (!forbiddenValues) {
-                throw new IllegalArgumentException(
-                        "The password contains forbidden values.");
-            }
-            return password;
         }
-        throw new IllegalArgumentException("Password is null.");
+        if (!hasDigit) {
+            throw new IllegalArgumentException(
+                    "The password must contain a digit.");
+        }
+        if (!hasLetterOrDigit) {
+            throw new IllegalArgumentException(
+                    "The password must contain special character.");
+        }
+        if (!forbiddenValues) {
+            throw new IllegalArgumentException(
+                    "The password contains forbidden values.");
+        }
+        return password;
     }
 }
